be0cd4a972081928c7ba6ff670f60823708c7795
Mark Tse <mark.tse@neverendingqs.com>
2017-01-08 15:29:19 -0500

    app - adding basic convert code.

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,61 @@
-var React = require('react');
+const React = require('react');
+const _ = require('lodash/string');
 
-module.exports = function() {
-  return <h1>First app</h1>;
-};
+function convertCase(s, convertType) {
+  switch(convertType) {
+    case "camel-case":
+      return _.camelCase(s);
+    case "kebab-case":
+      return _.kebabCase(s);
+    case "lower-case":
+      return _.lowerCase(s);
+    case "snake-case":
+      return _.snakeCase(s);
+    case "start-case":
+      return _.startCase(s);
+    case "upper-case":
+      return _.upperCase(s);
+    default:
+      return s;
+  }
+}
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      converted: '',
+      convertType: 'camel-case'
+    };
+  },
+
+  handleSelectionChange: function(e) {
+    this.setState({
+      convertType: e.target.value,
+      converted: convertCase(this.state.converted, e.target.value)
+    });
+
+  },
+
+  handleTextChange: function(e) {
+	  this.setState({
+      converted: convertCase(e.target.value, this.state.convertType)
+    });
+	},
+
+  render: function () {
+    return (
+      <div>
+        <input type="text" id="unconverted" onChange={this.handleTextChange} />
+        <select onChange={this.handleSelectionChange}>
+          <option value="camel-case">Camel Case</option>
+          <option value="kebab-case">Kebab Case</option>
+          <option value="lower-case">Lower Case</option>
+          <option value="snake-case">Snake Case</option>
+          <option value="start-case">Start Case</option>
+          <option value="upper-case">Upper Case</option>
+        </select>
+        <h1>{this.state.converted}</h1>
+      </div>
+    );
+  }
+});
