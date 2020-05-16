e1268a0b5ee65ef9440f701e47ac1e1c826f17fc
Mark Tse <mark.tse@neverendingqs.com>
2020-05-16 17:53:47 -0400

    app - adding base64 functions.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container">
+    <h1 class="title is-1">String Utilities</h1>
+    <h2 class="subtitle is-4">Handy string utilities!</h2>
+    <section>
+      <b-field>
+        <b-input
+          type="textarea"
+          rows="10"
+          v-model="input"
+          @input="updateOutput"
+        ></b-input>
+      </b-field>
+      <b-field>
+        <b-select
+          expanded
+          v-model="selectedFunction"
+        >
+          <option
+            v-for="[name] in Object.entries(functions)"
+            :value="name"
+            :key="name">
+            {{ name }}
+          </option>
+        </b-select>
+      </b-field>
+      <b-field>
+        <b-input
+          type="textarea"
+          rows="10"
+          v-model="output"
+        ></b-input>
+      </b-field>
+      <b-field>
+        <b-button type="is-primary">Copy to Clipboard</b-button>
+      </b-field>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      input: '',
+      functions: {
+        'Base64 Decode': btoa,
+        'Base64 Encode': atob
+      },
+      selectedFunction: 'Base64 Decode',
+      output: ''
+    }
+  },
+  methods: {
+    updateOutput() {
+      const f = this.functions[this.selectedFunction];
+      this.output = f(this.input);
+    }
   }
 };
 </script>
